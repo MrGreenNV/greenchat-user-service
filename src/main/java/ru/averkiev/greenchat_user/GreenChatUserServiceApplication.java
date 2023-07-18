fc52ad2dc27610991c8bc8b96156c5914f85b121
MrGreenNV <averkievnv@gmail.com>
2023-07-18 20:55:44 +0300

    init

---
+++ src/main/java/ru/averkiev/greenchat_user/GreenChatUserServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.averkiev.greenchat_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenChatUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenChatUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/averkiev/greenchat_user/GreenChatUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.averkiev.greenchat_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenChatUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
