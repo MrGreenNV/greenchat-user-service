b0b88576f0d22f75a3c37d33b994aeef5fba3f59
MrGreenNV <averkievnv@gmail.com>
2023-07-19 19:10:45 +0300

    update application.properties.origin

--- src/main/java/ru/averkiev/greenchat_user/GreenChatUserServiceApplication.java
+++ src/main/java/ru/averkiev/greenchat_user/GreenChatUserServiceApplication.java
@@ -3,9 +3,16 @@ package ru.averkiev.greenchat_user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Основной класс для запуска микросервиса.
+ */
 @SpringBootApplication
 public class GreenChatUserServiceApplication {
 
+    /**
+     * Запускает проект
+     * @param args - аргументы, для запуска приложения.
+     */
     public static void main(String[] args) {
         SpringApplication.run(GreenChatUserServiceApplication.class, args);
     }
