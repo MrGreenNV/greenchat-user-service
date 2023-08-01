95e1b64af52f8e3cebe756d1b4535da4db3ba5a8
MrGreenNV <averkievnv@gmail.com>
2023-08-01 21:32:33 +0300

    add UserAuthDTO.java

---
+++ src/main/java/ru/averkiev/greenchat_user/models/dto/user/UserAuthDTO.java
@@ -0,0 +1,55 @@
+package ru.averkiev.greenchat_user.models.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ru.averkiev.greenchat_user.models.Role;
+import ru.averkiev.greenchat_user.models.Status;
+
+import java.util.List;
+
+/**
+ * Класс DTO для представления данных для аутентификации пользователя.
+ * @author mrGreenNV
+ */
+@Getter
+@Setter
+@ResponseBody
+public class UserAuthDTO {
+
+    /**
+     * Имя пользователя для входа в систему - логин.
+     */
+    private String login;
+
+    /**
+     * Хэшированный пароль пользователя.
+     */
+    private String password;
+
+    /**
+     * Имя пользователя.
+     */
+    private String firstname;
+
+    /**
+     * Фамилия пользователя.
+     */
+    private String lastname;
+
+    /**
+     * Email пользователя.
+     */
+    private String email;
+
+    /**
+     * Статус пользователя в системе.
+     */
+    private Status status;
+
+    /**
+     * Список ролей пользователя.
+     */
+    private List<Role> roles;
+
+}
